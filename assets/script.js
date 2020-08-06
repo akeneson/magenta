24c2e43226114f35afd8b5d8a630e01c62c5a7e6
Zach Parr <zachery.parr@gmail.com>
2020-08-06 15:02:07 -0500

    home button and functionality

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,22 @@
 const randomBtn = $("#randomButton")
+const homeBtn = $("#homeButton")
 const boredAPIURL = "http://www.boredapi.com/api/"
+const boredRandomPath = "activity/"
 
-randomBtn.on("click", function () {
+
+// Random button function
+function randomBtnClick() {
     // Hide search area and show results area
     $("#searchArea").addClass("hide")
     $("#resultArea").removeClass("hide")
-    const boredRandomAPI = boredAPIURL + "activity/"
+
+    const boredRandomAPI = boredAPIURL + boredRandomPath
+    // Make API call
     $.ajax({
         url: boredRandomAPI,
         method: "GET",
     }).then(function (res) {
         // Query boredapi and get results
-
         // Activity
         let boredAPIActivityEl = $("<p>").text(res.activity)
         // Type
@@ -21,14 +26,33 @@ randomBtn.on("click", function () {
         let boredAPIParticipantsEL = $("<p>").text("Number of Participants: " + res.participants)
 
         // Price
-        let boredAPIPriceEL = $("<p>").text("Price:" + res.price)
+        let boredAPIPriceEL = $("<p>").text("Price: " + res.price)
+
+        // Home button
+        let homeBtn = $("<button>").attr({
+            class: "btn btn-secondary",
+            id: "homeButton"
+        })
+        let homeBtnEl = homeBtn.text("Go Home")
+
+        // Search again button
 
         // Add results to results area on page
-        let boredResults = $("<div>").append(boredAPIActivityEl, boredAPITypeEl, boredAPIParticipantsEL, boredAPIPriceEL)
+        let boredResults = $("<div>").append(boredAPIActivityEl, boredAPITypeEl, boredAPIParticipantsEL, boredAPIPriceEL, homeBtnEl)
         $("#resultArea").append(boredResults)
 
-
-
     })
+}
 
-})
+function homeBtnClick() {
+    // Hide result area and show search area
+    $("#resultArea").addClass("hide")
+    $("#searchArea").removeClass("hide")
+
+    // Clear result contents
+    $("#resultArea").empty()
+
+}
+
+randomBtn.on("click", randomBtnClick)
+$(document).on("click", "#homeButton", homeBtnClick)
