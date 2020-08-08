f19fbec83942ae1aa3a44b07aa130749cadc25b6
Zach Parr <zachery.parr@gmail.com>
2020-08-08 13:56:22 -0500

    adding social distance warning

--- assets/script.js
+++ assets/script.js
@@ -64,11 +64,64 @@ function homeBtnClick() {
 }
 
 function filteredSearch() {
-    searchType = custom
+    searchType = "custom"
+    // Hide search area and show results area
+    $("#searchArea").addClass("hide")
+    $("#resultArea").removeClass("hide")
 
+    // Clear result contents
+    $("#resultArea").empty()
+
+    // Custom search URL
     let activitySearchType = "type=" + activityType + "&"
     let activitySearchPeople = "participants=" + activityPeople
     let filteredSearchURL = boredAPIURL + "activity?" + activitySearchType + activitySearchPeople
+
+    // Make API call
+    $.ajax({
+        url: filteredSearchURL,
+        method: "GET",
+    }).then(function (res) {
+        // Query boredapi and get results
+        // Activity
+        // Home button
+        let homeBtn = $("<button>").attr({
+            class: "btn btn-secondary",
+            id: "homeButton"
+        })
+        let homeBtnEl = homeBtn.text("Go Home")
+
+        // Search again button
+        let retryBtn = $("<button>").attr({
+            class: "btn btn-secondary",
+            id: "retryButton"
+        })
+        let retryBtnEl = retryBtn.text("Look Again")
+        if (res.error) {
+            let errorMsg = $("<p>").text("No activity found, go back to the home page and try again...")
+
+            $("#resultArea").append(errorMsg, homeBtnEl)
+        } else {
+
+            let boredAPIActivityEl = $("<p>").text(res.activity)
+            // Type
+            let boredAPITypeEl = $("<p>").text("Activity Type: " + res.type)
+
+            // Participates
+            let boredAPIParticipantsEL = $("<p>").text("Number of Participants: " + res.participants)
+
+            // Price
+            let boredAPIPriceEL = $("<p>").text("Price: " + res.price)
+
+
+
+            // Add results to results area on page
+            let boredResults = $("<div>").append(boredAPIActivityEl, boredAPITypeEl, boredAPIParticipantsEL, boredAPIPriceEL, homeBtnEl, retryBtnEl)
+            $("#resultArea").append(boredResults)
+        }
+
+    })
+
 
 }
 
@@ -82,6 +135,8 @@ $(document).on("click", "#homeButton", homeBtnClick)
 $(document).on("click", "#retryButton", function () {
     if (searchType === "random") {
         randomBtnClick()
+    } else if (searchType === "custom") {
+        filteredSearch()
     }
 })
 
@@ -97,8 +152,25 @@ $(document).on("click", ".activityBtn", function () {
 
 // Listener for people count
 $(document).on("click", ".peopleBtn", function () {
-    if (activityPeople >= 4) {
-        activityPeople = $(this).text()
+
+    if (activityPeople = 4) {
+        // Hide search area and show results area
+        $("#searchArea").addClass("hide")
+        $("#resultArea").removeClass("hide")
+
+        // Clear result contents
+        $("#resultArea").empty()
+
+        // Home button
+        let homeBtn = $("<button>").attr({
+            class: "btn btn-secondary",
+            id: "homeButton"
+        })
+        let homeBtnEl = homeBtn.text("Go Home")
+
+        let warningMsg = $("<p>").text("You need to social distance go home and try again...")
+
+        $("#resultArea").append(warningMsg, homeBtnEl)
     } else {
         activityPeople = $(this).text()
     }
